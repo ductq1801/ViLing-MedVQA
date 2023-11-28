1c6cc3f1224c0a8bcb9a95edcdc916205ecb46d7
ductq <ductq@gmail.com>
2023-11-28 21:56:48 +0700

    new

--- net/model.py
+++ net/model.py
@@ -21,6 +21,22 @@ from net.initialization import init_weights
 from net.stm import STM
 def max_neg_value(t):
     return -torch.finfo(t.dtype).max
+class LayerNormalization(nn.Module):
+
+    def __init__(self, features: int, eps:float=10**-6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(features)) # alpha is a learnable parameter
+        self.bias = nn.Parameter(torch.zeros(features)) # bias is a learnable parameter
+
+    def forward(self, x):
+        # x: (batch, seq_len, hidden_size)
+         # Keep the dimension for broadcasting
+        mean = x.mean(dim = -1, keepdim = True) # (batch, seq_len, 1)
+        # Keep the dimension for broadcasting
+        std = x.std(dim = -1, keepdim = True) # (batch, seq_len, 1)
+        # eps is to prevent dividing by zero or when std is very small
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
 class LayerScale(nn.Module):
     def __init__(self, dim, depth, fn):
         super().__init__()
