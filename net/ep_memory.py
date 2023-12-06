977ce62cd709df2116988c645b409c1cb262bc8a
ductq <ductq@gmail.com>
2023-12-07 00:19:13 +0700

    new

--- net/ep_memory.py
+++ net/ep_memory.py
@@ -1,113 +1,166 @@
-import os
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.nn.init as init
 from torch.autograd import Variable
+from torch import nn
+from torch.nn.init import kaiming_uniform_, xavier_uniform_, normal
+import torch.nn.functional as F
 
-class AttentionGRUCell(nn.Module):
-    def __init__(self, input_size, hidden_size):
-        super(AttentionGRUCell, self).__init__()
-        self.hidden_size = hidden_size
-        self.Wr = nn.Linear(input_size, hidden_size)
-        init.xavier_normal(self.Wr.state_dict()['weight'])
-        self.Ur = nn.Linear(hidden_size, hidden_size)
-        init.xavier_normal(self.Ur.state_dict()['weight'])
-        self.W = nn.Linear(input_size, hidden_size)
-        init.xavier_normal(self.W.state_dict()['weight'])
-        self.U = nn.Linear(hidden_size, hidden_size)
-        init.xavier_normal(self.U.state_dict()['weight'])
+def linear(in_dim, out_dim, bias=True):
+    lin = nn.Linear(in_dim, out_dim, bias=bias)
+    xavier_uniform_(lin.weight)
+    if bias:
+        lin.bias.data.zero_()
 
-    def forward(self, fact, C, g):
-        '''
-        fact.size() -> (#batch, #hidden = #embedding)
-        c.size() -> (#hidden, ) -> (#batch, #hidden = #embedding)
-        r.size() -> (#batch, #hidden = #embedding)
-        h_tilda.size() -> (#batch, #hidden = #embedding)
-        g.size() -> (#batch, )
-        '''
+    return lin
 
-        r = F.sigmoid(self.Wr(fact) + self.Ur(C))
-        h_tilda = F.tanh(self.W(fact) + r * self.U(C))
-        g = g.unsqueeze(1).expand_as(h_tilda)
-        h = g * h_tilda + (1 - g) * C
-        return h
+class ControlUnit(nn.Module):
+    def __init__(self, dim, max_step):
+        super().__init__()
 
-class AttentionGRU(nn.Module):
-    def __init__(self, input_size, hidden_size):
-        super(AttentionGRU, self).__init__()
-        self.hidden_size = hidden_size
-        self.AGRUCell = AttentionGRUCell(input_size, hidden_size)
+        self.position_aware = nn.ModuleList()
+        for i in range(max_step):
+            self.position_aware.append(linear(dim, dim))
 
-    def forward(self, facts, G):
-        '''
-        facts.size() -> (#batch, #sentence, #hidden = #embedding)
-        fact.size() -> (#batch, #hidden = #embedding)
-        G.size() -> (#batch, #sentence)
-        g.size() -> (#batch, )
-        C.size() -> (#batch, #hidden)
-        '''
-        batch_num, sen_num, embedding_size = facts.size()
-        C = Variable(torch.zeros(self.hidden_size)).cuda()
-        for sid in range(sen_num):
-            fact = facts[:, sid, :]
-            g = G[:, sid]
-            if sid == 0:
-                C = C.unsqueeze(0).expand_as(fact)
-            C = self.AGRUCell(fact, C, g)
-        return C
+        self.control_question = linear(2*dim, dim)
+        self.context_proj = nn.Parameter(torch.rand((30,169)))
+        self.attn = linear(dim, 1)
 
-class EpisodicMemory(nn.Module):
-    def __init__(self, hidden_size):
-        super(EpisodicMemory, self).__init__()
-        self.AGRU = AttentionGRU(hidden_size, hidden_size)
-        self.z1 = nn.Linear(4 * hidden_size, hidden_size)
-        self.z2 = nn.Linear(hidden_size, 1)
-        self.next_mem = nn.Linear(3 * hidden_size, hidden_size)
-        init.xavier_normal(self.z1.state_dict()['weight'])
-        init.xavier_normal(self.z2.state_dict()['weight'])
-        init.xavier_normal(self.next_mem.state_dict()['weight'])
+        self.dim = dim
 
-    def make_interaction(self, facts, questions, prevM):
-        '''
-        facts.size() -> (#batch, #sentence, #hidden = #embedding)
-        questions.size() -> (#batch, 1, #hidden)
-        prevM.size() -> (#batch, #sentence = 1, #hidden = #embedding)
-        z.size() -> (#batch, #sentence, 4 x #embedding)
-        G.size() -> (#batch, #sentence)
-        '''
-        batch_num, sen_num, embedding_size = facts.size()
-        questions = questions.expand_as(facts)
-        prevM = prevM.expand_as(facts)
+    def forward(self, step, context, question, control):
+        position_aware = self.position_aware[step](question)
 
-        z = torch.cat([
-            facts * questions,
-            facts * prevM,
-            torch.abs(facts - questions),
-            torch.abs(facts - prevM)
-        ], dim=2)
+        control_question = torch.cat([control, position_aware], 2)
+        control_question = self.control_question(control_question)
+        p_context = torch.einsum('bnc,vn->bvc',context,self.context_proj)
 
-        z = z.view(-1, 4 * embedding_size)
+        context_prod = control_question * p_context
+        attn_weight = self.attn(context_prod)
 
-        G = F.tanh(self.z1(z))
-        G = self.z2(G)
-        G = G.view(batch_num, -1)
-        G = F.softmax(G)
+        attn = F.softmax(attn_weight, 1)
 
-        return G
+        next_control = (attn * p_context)
 
-    def forward(self, facts, questions, prevM):
-        '''
-        facts.size() -> (#batch, #sentence, #hidden = #embedding)
-        questions.size() -> (#batch, #sentence = 1, #hidden)
-        prevM.size() -> (#batch, #sentence = 1, #hidden = #embedding)
-        G.size() -> (#batch, #sentence)
-        C.size() -> (#batch, #hidden)
-        concat.size() -> (#batch, 3 x #embedding)
-        '''
-        G = self.make_interaction(facts, questions, prevM)
-        C = self.AGRU(facts, G)
-        concat = torch.cat([prevM.squeeze(1), C, questions.squeeze(1)], dim=1)
-        next_mem = F.relu(self.next_mem(concat))
-        next_mem = next_mem.unsqueeze(1)
+        return next_control
+
+
+class ReadUnit(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+
+        self.mem = linear(dim, dim)
+        self.concat = linear(dim * 2, dim)
+        self.attn = linear(dim, dim)
+        self.know_proj = nn.Parameter(torch.rand((30,169)))
+    def forward(self, memory, know, control):
+
+        mem = self.mem(memory)
+        know_p = torch.einsum('bnc,vn->bvc',know,self.know_proj)
+
+        concat = self.concat(torch.cat([mem * know_p, know_p], 2))
+
+        attn = concat * control
+
+        attn = self.attn(attn).squeeze(2)
+
+        attn = F.softmax(attn, 1).unsqueeze(1)
+
+        read = (attn * know_p).sum(1)
+
+        return read
+
+
+class WriteUnit(nn.Module):
+    def __init__(self, dim, self_attention=False, memory_gate=False):
+        super().__init__()
+
+        self.concat = linear(dim * 2, dim)
+
+        if self_attention:
+            self.attn = linear(dim, 1)
+            self.mem = linear(dim, dim)
+
+        if memory_gate:
+            self.control = linear(dim, 1)
+
+        self.self_attention = self_attention
+        self.memory_gate = memory_gate
+
+    def forward(self, memories, retrieved, controls):
+        prev_mem = memories
+        print(retrieved.shape)
+        print(prev_mem.shape)
+        concat = self.concat(torch.cat([retrieved, prev_mem], 2))
+        next_mem = concat
+
+        if self.self_attention:
+            controls_cat = torch.stack(controls[:-1], 2)
+            attn = controls[-1].unsqueeze(2) * controls_cat
+            attn = self.attn(attn.permute(0, 2, 1))
+            attn = F.softmax(attn, 1).permute(0, 2, 1)
+
+            memories_cat = torch.stack(memories, 2)
+            attn_mem = (attn * memories_cat).sum(2)
+            next_mem = self.mem(attn_mem) + concat
+
+        if self.memory_gate:
+            control = self.control(controls[-1])
+            gate = F.sigmoid(control)
+            next_mem = gate * prev_mem + (1 - gate) * next_mem
+
         return next_mem
+
+
+class MACUnit(nn.Module):
+    def __init__(self, dim,mem = 30, max_step=12,
+                self_attention=False, memory_gate=False,
+                dropout=0.15):
+        super().__init__()
+
+        self.control = ControlUnit(dim, max_step)
+        self.read = ReadUnit(dim)
+        self.write = WriteUnit(dim, self_attention, memory_gate)
+
+        self.mem_0 = nn.Parameter(torch.zeros(1,mem, dim))
+        self.control_0 = nn.Parameter(torch.zeros(1,mem, dim))
+
+        self.dim = dim
+        self.max_step = max_step
+        self.dropout = dropout
+        self.mem = mem
+    def get_mask(self, x, dropout):
+        mask = torch.empty_like(x).bernoulli_(1 - dropout)
+        mask = mask / (1 - dropout)
+
+        return mask
+
+    def forward(self, context, question, knowledge):
+        b_size = question.size(0)
+
+        control = self.control_0.expand(b_size,self.mem, self.dim)
+        memory = self.mem_0.expand(b_size, self.mem,self.dim)
+
+        if self.training:
+            control_mask = self.get_mask(control, self.dropout)
+            memory_mask = self.get_mask(memory, self.dropout)
+            control = control * control_mask
+            memory = memory * memory_mask
+
+        controls = control
+        memories = memory
+
+        for i in range(self.max_step):
+            control = self.control(i, context, question, control)
+            if self.training:
+                control_mask = self.get_mask(control, self.dropout)
+                control = control * control_mask
+              
+            controls=controls + control
+
+            read = self.read(memories, knowledge, controls)
+            print(read.shape)
+            memory = self.write(memories, read, controls)
+            if self.training:
+                memory = memory * memory_mask
+            memories= memories + memory
+
+        return memory
